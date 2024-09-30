27f6e9fe0ef780fcba4a138fdd743ac7f29d3fa9
Arturo Muller <amullersimons@gmail.com>
2024-09-30 18:15:45 -0400

    feat: webApp sdk

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { TonConnectButton } from "@tonconnect/ui-react";
 import { useMainContract } from "./hooks/useMainContract.ts";
 import { useTonConnect } from "./hooks/useTonConnect.ts";
 import { fromNano } from "ton-core";
+import WebApp from '@twa-dev/sdk';
 
 function App() {
   const {
@@ -15,6 +16,11 @@ function App() {
   } = useMainContract();
 
   const { connected } = useTonConnect();
+
+  const showAlert = () => {
+    WebApp.showAlert("Hey there!");
+  };
+
   return (
     <div>
       <div>
@@ -22,11 +28,12 @@ function App() {
       </div>
       <div>
         <div className="Card">
+          <b>{WebApp.platform}</b>
           <b>Our contract Address</b>
           <div className="Hint">{contract_address?.slice(0, 30) + "..."}</div>
           <b>Our contract Balance</b>
           {contract_balance && (
-            <div className="Hint">{fromNano(contract_balance)}</div>
+              <div className="Hint">{fromNano(contract_balance)}</div>
           )}
         </div>
 
@@ -34,15 +41,25 @@ function App() {
           <b>Counter Value</b>
           <div>{counter_value ?? "Loading..."}</div>
         </div>
+        <a
+            onClick={() => {
+              showAlert();
+            }}
+        >
+          Show Alert
+        </a>
+
+        <br/>
+
         {connected && <a onClick={() => sendIncrement()}>Increment by 5</a>}
-        <br />
+        <br/>
         {connected && (
-          <a onClick={() => sendDeposit()}>request deposit of 1 ton</a>
+            <a onClick={() => sendDeposit()}>request deposit of 1 ton</a>
         )}
         {connected && (
-          <a onClick={() => sendWithdrawalRequest()}>
-            request withdrawal of 0.7 ton
-          </a>
+            <a onClick={() => sendWithdrawalRequest()}>
+              request withdrawal of 0.7 ton
+            </a>
         )}
       </div>
     </div>
