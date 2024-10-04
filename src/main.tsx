129cd568a965d8ad771f5b0a7f782c19b9368313
Arturo Muller <amullersimons@gmail.com>
2024-10-04 18:30:55 -0400

    update well

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 // this manifest is used temporarily for development purposes
 const manifestUrl =
-  "https://http://ArturoMuller.github.io/first_contract_front_end/tonconnect-manifest.json";
+  "https://http://ArturoMuller.github.io/first_contract_front_end_2/tonconnect-manifest.json";
 
 createRoot(document.getElementById("root")!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
